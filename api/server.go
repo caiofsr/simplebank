b0ae06efcb61f21b2ec2b0ad8924c44a62e404f0
Caio Fernando <caiofsr13@gmail.com>
2022-08-24 23:18:21 -0300

    feat(user): :sparkles: user login

--- api/server.go
+++ api/server.go
@@ -1,24 +1,53 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/caiofsr/simplebank/db/sqlc"
+	"github.com/caiofsr/simplebank/token"
+	"github.com/caiofsr/simplebank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+
+	server.setupRouter()
+
+	return server, nil
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -28,15 +57,7 @@ func NewServer(store db.Store) *Server {
 
 	router.POST("/users", server.createUser)
 
+	router.POST("/auth/login", server.loginUser)
+
 	server.router = router
-
-	return server
-}
-
-func (server *Server) Start(address string) error {
-	return server.router.Run(address)
-}
-
-func errorResponse(err error) gin.H {
-	return gin.H{"error": err.Error()}
 }
